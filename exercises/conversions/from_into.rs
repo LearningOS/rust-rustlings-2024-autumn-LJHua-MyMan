9e6f245fd7650d33e986cdbcc894ddb5a5fa8682
LJHua-MyMan <1528255673@qq.com>
2024-10-18 17:18:42 +0800

    finish and remember retry algorithm

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,22 +40,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
         let v = s.split(',').collect::<Vec<&str>>();
         if v.len() == 2 && v[0].len() > 0 && v[1].len() > 0{
             let name_ = v[0].to_string();
-            let age_ = v[1].parse::<usize>().unwrap_or(0);
-            if name_.len() > 0 && age_.len() > 0 {
-                Person{
-                    name:name_,
-                    age:age_
+            match v[1].parse::<usize>() {
+                    Ok(age_)  => Person {
+                        name: name_,
+                        age: age_,
+                    },
+                    _ => Person::default(),
                 }
-            }else{
-                Person::default()
-            }
         }else{
             Person::default()
         }
